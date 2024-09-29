5e0dccc365a62cdd6d06058f2d9e67da7302fc31
Nick Hinds <nicholashinds2@gmail.com>
2024-09-29 18:37:25 -0500

    #21: chakra ui setup

---
+++ web-app/src/app/theme.tsx
@@ -0,0 +1,11 @@
+// src/app/theme.tsx
+"use client";
+
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+export const theme = extendTheme({});
