5cb52cc66261205b960c6daa146b55049f648315
Danny Bui <Dannybui5155@gmail.com>
2024-10-16 23:16:56 -0500

    #48: Preliminary auth, route, api and pages setup

--- web-app/src/app/dev/sandbox/page.tsx
+++ web-app/src/app/dev/sandbox/page.tsx
@@ -1,7 +1,6 @@
 import { Box, Heading } from "@chakra-ui/react";
-import Link from "next/link";
 
-export default function Home() {
+export default function Sandbox() {
   return (
     <Box bg="brand.50" maxW="container.xl" mx="auto" px={5}>
       <Heading
@@ -11,7 +10,7 @@ export default function Home() {
         mb={5}
         mt={{ base: "none", md: "30" }}
       >
-        <Link href="/dev/sandbox">Gibberle</Link>
+        Sandbox
       </Heading>
     </Box>
   );
