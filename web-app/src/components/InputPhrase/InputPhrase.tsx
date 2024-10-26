92c84dc013b76c1820961ca0c48298e9f1c782df
Ethan Harris <ethanlaingharris@icloud.com>
2024-10-26 18:09:09 -0500

    #75: Input string and caret now automatically detect spaces

--- web-app/src/components/InputPhrase/InputPhrase.tsx
+++ web-app/src/components/InputPhrase/InputPhrase.tsx
@@ -9,6 +9,7 @@ export const InputPhrase: React.FC<InputPhraseProp> = ({correctPhrase}) => {
 
     const [inputStr, setInputStr] = useState<string>("");
     const [caretStr, setCaretStr] = useState<string>("_");
+    const [inputIndex, setInputIndex] = useState<number>(0);
 
     const blankPhrase = correctPhrase
         .split("")
@@ -16,15 +17,32 @@ export const InputPhrase: React.FC<InputPhraseProp> = ({correctPhrase}) => {
         .join("");
     
     const handleKeyDown = (event: KeyboardEvent) => {
+        // NOTE: "\u00A0" is a non-breaking space, if you just do " ",
+        // it collapses the spaces so need to use it 
+        //  when adding spaces to inputStr and caretStr
         const key = event.key.toLowerCase();
-        if ("qwertyuiopasdfghjklzxcvbnm".includes(key)) {
-            setInputStr((prev) => prev + key);
-            // "\u00A0" is a non-breaking space, if you just do " ",
-            // it collapses the spaces so nothing happens to the caretStr variable
-            setCaretStr((prev) => "\u00A0" + prev);
-        } else if (key === "backspace") {
-            setInputStr((prev) => prev.slice(0, -1));
-            setCaretStr((prev) => prev.slice(1));
+        if ("qwertyuiopasdfghjklzxcvbnm".includes(key) && (inputStr.length < correctPhrase.length)) {
+            if (correctPhrase[inputIndex + 1] === " ") {
+                // Next character after the cursor is a space, so add a space automatically
+                setInputStr((prev) => prev + key + "\u00A0");
+                setCaretStr((prev) => "\u00A0" + "\u00A0" + prev);
+                setInputIndex((prev) => prev + 2);
+            } else {
+                setInputStr((prev) => prev + key);
+                setCaretStr((prev) => "\u00A0" + prev);
+                setInputIndex((prev) => prev + 1);
+            }
+        } else if (key === "backspace" && (inputStr.length > 0)) {
+            if (inputStr[inputIndex - 1] === "\u00A0") {
+                // previouse character is a space, so automatically delete space
+                setInputStr((prev) => prev.slice(0, -2));
+                setCaretStr((prev) => prev.slice(2));
+                setInputIndex((prev) => prev - 2);
+            } else {
+                setInputStr((prev) => prev.slice(0, -1));
+                setCaretStr((prev) => prev.slice(1));
+                setInputIndex((prev) => prev - 1);
+            }
         }
     };
 
@@ -33,7 +51,7 @@ export const InputPhrase: React.FC<InputPhraseProp> = ({correctPhrase}) => {
         return () => {
             window.removeEventListener("keydown", handleKeyDown);
         }
-    }, []);
+    }, [inputIndex, inputStr]);
 
     return (
         <Box position='relative' display='inline-block'>
@@ -46,7 +64,6 @@ export const InputPhrase: React.FC<InputPhraseProp> = ({correctPhrase}) => {
                 left={0}
                 color='#EAEAEA' // Adjust to desired text color
                 textAlign='left'
-                boxShadow='0 0 2px red'
             >
                 {inputStr}
             </Text>
