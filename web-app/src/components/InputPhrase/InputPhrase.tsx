f3d2ae71c7e39acce44cb246c26b0ef7e62d1bfa
Ethan Harris <ethanlaingharris@icloud.com>
2024-10-26 12:50:55 -0500

    #75: Input string correctly responds to key and baackspace input, doesn not automatically respond to spaces

--- web-app/src/components/InputPhrase/InputPhrase.tsx
+++ web-app/src/components/InputPhrase/InputPhrase.tsx
@@ -1,12 +1,40 @@
+"use client";  // Needed this line so useState, and useEffect work
 import { InputCursor } from "@/components/InputCursor/InputCursor";
 import { Box, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { InputPhraseProp } from "./types";
 
-export const InputPhrase: React.FC<InputPhraseProp> = ({inputStr="", correctPhrase}) => {
+export const InputPhrase: React.FC<InputPhraseProp> = ({correctPhrase}) => {
+    // variables for tracking characters per word and which word the user is at
+
+    const [inputStr, setInputStr] = useState<string>("");
+    const [caretStr, setCaretStr] = useState<string>("_");
+
     const blankPhrase = correctPhrase
         .split("")
         .map((char) => (char === " " ? " " : "_"))
         .join("");
+    
+    const handleKeyDown = (event: KeyboardEvent) => {
+        const key = event.key.toLowerCase();
+        if ("qwertyuiopasdfghjklzxcvbnm".includes(key)) {
+            setInputStr((prev) => prev + key);
+            // "\u00A0" is a non-breaking space, if you just do " ",
+            // it collapses the spaces so nothing happens to the caretStr variable
+            setCaretStr((prev) => "\u00A0" + prev);
+        } else if (key === "backspace") {
+            setInputStr((prev) => prev.slice(0, -1));
+            setCaretStr((prev) => prev.slice(1));
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        }
+    }, []);
+
     return (
         <Box position='relative' display='inline-block'>
             <Text
@@ -14,7 +42,7 @@ export const InputPhrase: React.FC<InputPhraseProp> = ({inputStr="", correctPhra
                 fontSize={40}
                 letterSpacing='0.75rem'
                 position='absolute'
-                top='0.5rem' // Adjust this value to control the height offset
+                top='0.3rem' // Adjust this value to control the height offset
                 left={0}
                 color='#EAEAEA' // Adjust to desired text color
                 textAlign='left'
@@ -31,7 +59,7 @@ export const InputPhrase: React.FC<InputPhraseProp> = ({inputStr="", correctPhra
                 {blankPhrase}
             </Text>
             <Box position='absolute' top={0} left={0}>
-                <InputCursor />
+                <InputCursor cursorStr={caretStr} />
             </Box>
         </Box>
     );
--- web-app/src/components/InputPhrase/types.ts
+++ web-app/src/components/InputPhrase/types.ts
@@ -1,4 +1,3 @@
 export interface InputPhraseProp {
-    inputStr?: string;    // Starts at as an empty string ""
     correctPhrase: string;
 }
